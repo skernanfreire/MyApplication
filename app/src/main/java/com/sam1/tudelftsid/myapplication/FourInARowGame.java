757059b36a93ca48cc773784ba92b06c4931ac7b
skernanfreire <skernanfreire@gmail.com>
2018-02-19 14:44:32 +0100

    Initial commit

---
+++ app/src/main/java/com/sam1/tudelftsid/myapplication/FourInARowGame.java
@@ -0,0 +1,163 @@
+package com.sam1.tudelftsid.myapplication;
+
+/**
+ * Created by TUDelft SID on 16-Feb-18.
+ */
+
+public class FourInARowGame {
+
+    private int[] numberOfEmptyColumnCells={6,6,6,6,6,6,6};
+    private int[][] board = new int[4][2];
+    private int playerId = 1, playerWhoWon = 0,emptyColumnCells,column;
+    private int[][] winningDiscs = new int[4][2];
+    private int counter = 0;
+
+    private void showBoard() {
+        System.out.println("Four in a row game board:");
+        for (int rowindex=0;rowindex<6;rowindex++) {
+            for(int columnindex=0;columnindex<7;columnindex++)
+                System.out.print(String.valueOf(board[rowindex][columnindex]));
+            System.out.println();
+        }
+    }
+
+    private boolean currentPlayerWon() {
+       // if (fourInARowHorizontally()||fourInARowVertically()||fourInARowReverseDiagonally()||fourInARowDiagonally()) return true;
+        if (fourInARowHorizontally()) return true;
+        //if (fourInARowVertically()) return true;
+       // if (fourInARowDiagonally()) return true;
+       // if (fourInARowReverseDiagonally()) return true;
+        return false;
+    }
+    private boolean isBoardFull() {
+        for (int columnindex = 6;columnindex>=0;columnindex--)
+            if (numberOfEmptyColumnCells[columnindex]>0)
+                return false;
+        return true;
+    }
+    private void checkForEndOfGame() {
+        if (currentPlayerWon()) playerWhoWon = playerId;
+        else
+        if (isBoardFull()) playerWhoWon = 3;
+        System.out.println("player who won: "+playerWhoWon+".");
+    }
+        // public methods
+        public int getResult() {
+            return playerWhoWon;
+    }
+    private void registerDisc(int rowindex, int columnindex){
+        if (winningDiscs==null)winningDiscs=new int[4][2];
+        winningDiscs[counter][0] = rowindex;
+        winningDiscs[counter][1] = columnindex;
+        counter++;
+    }
+    // checkFOrEndOfGame() helper methods
+    private boolean fourInARowHorizontally(){
+        int lefttofind=3, rowindex=emptyColumnCells-1;
+        // check to the left
+        int columnindex=column-2; // first column index to the left
+        registerDisc(rowindex,columnindex+1);
+        while (lefttofind>0 && columnindex>=0 && board[rowindex][columnindex]==playerId) {
+            registerDisc(rowindex,columnindex);
+            columnindex--;
+            lefttofind--;
+        }
+        if (lefttofind==0) return true;
+
+        // check to the right
+        columnindex=column; // first column index to the right
+        while (lefttofind>0 && columnindex<=6 && board[rowindex][columnindex]==playerId) {
+            registerDisc(rowindex,columnindex);
+            columnindex++;
+            lefttofind--;
+        }
+
+        if (lefttofind!=0) winningDiscs = null;
+
+        return (lefttofind==0);
+    }
+
+    public int[][] getWinningDiscs() {
+        return winningDiscs;
+    }
+
+    private boolean fourInARowVertically(){
+        int lefttofind=3, columnindex=column-1;
+        // check up
+        int rowindex=emptyColumnCells-2;
+        while (lefttofind>0 && rowindex>=0 && board[rowindex][columnindex]==playerId) {
+            rowindex--;
+            lefttofind--;
+        }
+
+        if (lefttofind==0) return true;
+        // check down
+        rowindex=emptyColumnCells; // first column index to the right
+        while (lefttofind>0 && rowindex<=5 && board[rowindex][columnindex]==playerId) {
+            rowindex++;
+            lefttofind--;
+        }
+        return (lefttofind==0);
+    }
+
+    private boolean fourInARowDiagonally() {
+        int lefttofind=3, rowindex, columnindex;
+        // check left and up
+        rowindex=emptyColumnCells-2; columnindex=column-2;
+        while (lefttofind>0 && rowindex>=0 && columnindex>=0 &&
+                board[rowindex][columnindex]==playerId) {
+            rowindex--;
+            columnindex--;
+            lefttofind--;
+        }
+        if (lefttofind==0) return true;
+        // check right and down
+        rowindex=emptyColumnCells; columnindex=column;
+        while (lefttofind>0 && rowindex<=5 && columnindex<=6 &&
+                board[rowindex][columnindex]==playerId) {
+            rowindex++;
+            columnindex++;
+            lefttofind--;
+        }
+        return (lefttofind==0);
+    }
+    private boolean fourInARowReverseDiagonally() {
+        int lefttofind=3, rowindex, columnindex;
+        // check right and up
+        rowindex=emptyColumnCells-2; columnindex=column;
+        while (lefttofind>0 && rowindex>=0 && columnindex<=6 &&
+                board[rowindex][columnindex]==playerId) {
+            rowindex--;
+            columnindex++;
+            lefttofind--;
+        }
+        if (lefttofind==0) return true;
+        // check left and down
+        rowindex=emptyColumnCells; columnindex=column-2;
+        while (lefttofind>0 && rowindex<=5 && columnindex>=0 &&
+                board[rowindex][columnindex]==playerId) {
+            rowindex++;
+            columnindex--;
+            lefttofind--;
+        }
+        return (lefttofind==0);
+    }
+
+    public int dropDisc(int column){
+        if (column>=1 && column<=7){
+            this.column = column;
+            emptyColumnCells=numberOfEmptyColumnCells[column-1]; // get number of empty column cells
+            if (emptyColumnCells>0) { // column is NOT full yet
+                numberOfEmptyColumnCells[column - 1] = emptyColumnCells - 1; //one less to go
+                board[emptyColumnCells - 1][column - 1] = playerId;
+                checkForEndOfGame();
+                showBoard();
+                playerId = (playerId == 1 ? 2 : 1);
+            }
+            return emptyColumnCells; // return the number of empty column cells we started out with
+        }
+        // some error
+        return 0;
+    }
+
+}
---
+++ app/src/main/java/com/sam1/tudelftsid/myapplication/MainActivity.java
@@ -0,0 +1,102 @@
+package com.sam1.tudelftsid.myapplication;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.os.Handler;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Handler blinker=new Handler();
+    private class Blinker implements Runnable{
+        public void run() {
+            // blinking
+            int j = 0;
+            while (j < 4) {
+                ImageView cell = getBoardImageView((winningDiscs[j][0]),(winningDiscs[j][1])); // TO DO
+                cell.setVisibility(cell.getVisibility() == View.INVISIBLE ? View.VISIBLE : View.INVISIBLE);
+                blinker.postDelayed(this, 400);
+                j++;
+            }
+        }
+    }
+    private TextView infoTextView;
+    private TableLayout boardTableLayout;
+    private int [][] winningDiscs = new int[4][2];
+    int column;
+    int mycolor= Color.RED;
+    Button columnButton;
+    private FourInARowGame fourInARowGame= new FourInARowGame();
+
+    public void info(String info){
+        infoTextView.setText(info);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        infoTextView = (TextView) findViewById(R.id.info_textview);
+        boardTableLayout = (TableLayout)findViewById(R.id.board_tablelayout);
+        info("Player 1 to play first!");
+    }
+    private ImageView getBoardImageView(int rowIndex, int columnIndex){
+        TableRow tableRow=(TableRow)boardTableLayout.getChildAt(rowIndex);
+        return (ImageView)tableRow.getChildAt(columnIndex);
+    }
+
+    private void showDisc(int row, int column, int color){
+        // set the background colour
+        getBoardImageView(row-1,column-1).setBackgroundColor(color);
+    }
+    private void endOfGame(int result) {
+        // disable all buttons
+        TableRow tableButtonRow = (TableRow) boardTableLayout.getChildAt(6);
+        for (int columnindex = 6; columnindex >= 0; columnindex--)
+            tableButtonRow.getChildAt(columnindex).setEnabled(false);
+        // inform user
+        if (result == 3) info("Nobody won!");
+        else{
+            winningDiscs = fourInARowGame.getWinningDiscs();
+            info("Player " + result + " won!");
+            blinker.post(new Blinker());
+        }
+    }
+
+
+    public void columnButtonClicked(View view){
+        columnButton=(Button)view;
+        column=Integer.parseInt(columnButton.getText().toString());
+        // further code to make the move and process the row returned
+        int row = fourInARowGame.dropDisc(column);
+        /*
+        while(row>0 && ((ColorDrawable)getBoardImageView(row-1,
+                column-1).getBackground()).getColor()!=Color.parseColor("#bbbbbb"))
+            row--; //decrement row
+        */
+        if (row==1) columnButton.setEnabled(false);
+        showDisc(row,column,mycolor);
+        int gameResult = fourInARowGame.getResult();
+        if (gameResult>0) {
+            endOfGame(gameResult);
+            // add blinkers
+            return;
+        }
+        if (mycolor==Color.RED){
+            //showDisc(row, column, mycolor);
+            mycolor=Color.BLUE;
+            info("Next move by Player2");
+        } else {
+            //showDisc(row,column,mycolor);
+            mycolor=Color.RED;
+            info("Next move by Player1");
+        }
+    }
+}
